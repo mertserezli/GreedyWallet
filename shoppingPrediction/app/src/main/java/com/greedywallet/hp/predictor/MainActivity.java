49709f40b909357a2fe3e06d073060c74fa2ddb1
Sertan Özcan <sertan_821@hotmail.com>
2019-01-30 20:11:45 +0300

    Create template for prediction view

---
+++ shoppingPrediction/app/src/main/java/com/greedywallet/hp/predictor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.greedywallet.hp.predictor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
