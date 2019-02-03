eddf188652fcb393f43e915e0981fa28e92ab960
Mert Serezli <mertserezli@gmail.com>
2019-02-04 00:04:26 +0300

    restructure project

--- ocr-reader-complete/app/src/main/java/com/halfbyte/greedywallet/AddManually.java
+++ ocr-reader-complete/app/src/main/java/com/halfbyte/greedywallet/AddManually.java
@@ -1,9 +1,9 @@
-package com.google.android.gms.samples.vision.ocrreader;
+package com.halfbyte.greedywallet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.samples.vision.ocrreader.R;
+import com.halfbyte.greedywallet.R;
 
 public class AddManually extends AppCompatActivity {
     public void onCreate(Bundle bundle) {
