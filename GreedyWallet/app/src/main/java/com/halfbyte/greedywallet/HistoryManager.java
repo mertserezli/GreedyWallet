8f59bc1c28cd7fb1bb0de421168fd8b5fbed12de
Sertan Özcan <sertan_821@hotmail.com>
2019-03-10 15:27:50 +0300

    Wrote HistoryManager for singleton database access to purchase history

---
+++ GreedyWallet/app/src/main/java/com/halfbyte/greedywallet/HistoryManager.java
@@ -0,0 +1,51 @@
+package com.halfbyte.greedywallet;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HistoryManager {
+    private static HistoryManager historyManager = new HistoryManager();
+    private Map<String, ArrayList<String>> history = new ConcurrentHashMap<>();
+
+    private HistoryManager(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference().child("itemsBoughtHistory");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String itemName = ds.child("item").child("isim").getValue().toString();
+                    String purchaseDate = ds.child("date").getValue().toString();
+
+                    if (history.containsKey(itemName)) {
+                        history.get(itemName).add(purchaseDate);
+                    } else {
+                        history.put(itemName, new ArrayList<String>(Arrays.asList(purchaseDate)));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    public static HistoryManager getInstance(){
+        return historyManager;
+    }
+
+    public Set<String> getItems(){return history.keySet();}
+
+    public ArrayList<String> getItemPurchaseDates(String item){return history.get(item);}
+}
--- GreedyWallet/app/src/main/java/com/halfbyte/greedywallet/ShowHistory.java
+++ GreedyWallet/app/src/main/java/com/halfbyte/greedywallet/ShowHistory.java
@@ -3,18 +3,8 @@ package com.halfbyte.greedywallet;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
-import com.google.firebase.database.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ShowHistory extends AppCompatActivity {
 
@@ -24,46 +14,20 @@ public class ShowHistory extends AppCompatActivity {
         setContentView(R.layout.showhistory);
 
         Context ctxt = this;
-        ViewGroup screen = (ViewGroup)findViewById(R.id.mainview);
+        ViewGroup screen = (ViewGroup) findViewById(R.id.mainview);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference().child("itemsBoughtHistory");
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Map<String, ArrayList<String>> history = new HashMap<>();
+        for (String item : HistoryManager.getInstance().getItems()) {
+            TextView itemView = new TextView(ctxt);
+            itemView.setText(item);
 
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String itemName = ds.child("item").child("isim").getValue().toString();
-                    String purchaseDate = ds.child("date").getValue().toString();
+            for (String purchaseDate : HistoryManager.getInstance().getItemPurchaseDates(item)) {
+                TextView purchaseDateView = new TextView(ctxt);
+                purchaseDateView.setText(purchaseDate);
 
-                    if(history.containsKey(itemName)) {
-                        history.get(itemName).add(purchaseDate);
-                    }
-                    else {
-                        history.put(itemName, new ArrayList<String>(Arrays.asList(purchaseDate)));
-                    }
-                }
-
-                for(String item : history.keySet()) {
-                    TextView itemView = new TextView(ctxt);
-                    itemView.setText(item);
-
-                    for(String purchaseDate : history.get(item)) {
-                        TextView purchaseDateView = new TextView(ctxt);
-                        purchaseDateView.setText(purchaseDate);
-
-                        screen.addView(purchaseDateView);
-                    }
-
-                    screen.addView(itemView);
-                }
+                screen.addView(purchaseDateView);
             }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // ...
-            }
-        });
+            screen.addView(itemView);
+        }
     }
 }
